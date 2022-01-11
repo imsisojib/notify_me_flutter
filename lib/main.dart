1b895309496cd4602f77803818af3cc1d009476c
sirajul.sojib <sirajul.sojib@leads-bd.com>
2022-01-11 23:02:27 +0600

    firebase initialization and default notification channel created.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-void main() {
+//for default notification channel
+const AndroidNotificationChannel channel = AndroidNotificationChannel(
+    "default_channel",
+  "NotifyMe Notifications",
+  importance: Importance.defaultImportance,
+  playSound: false,
+);
+
+//for local notification globally
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+//for background handling, if any notification appeared and app in background
+Future<void> firebaseNotificaionBackgroundHandler(RemoteMessage message) async{
+  await Firebase.initializeApp();
+}
+
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+
   runApp(MyApp());
 }
 
